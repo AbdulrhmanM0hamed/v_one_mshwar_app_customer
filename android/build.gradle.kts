c7a251d5b9b982683e2d62fcd4a6d92a4b7832c9
AbdulrhmanM0hamed <bodamohmed60@gmail.com>
2026-02-23 14:16:08 +0200

    run project

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -13,8 +13,35 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    val projectRoot = project.projectDir.toPath().root
+    val buildRoot = newSubprojectBuildDir.asFile.toPath().root
+    if (projectRoot == buildRoot) {
+        project.layout.buildDirectory.value(newSubprojectBuildDir)
+    }
 }
+
+subprojects {
+    afterEvaluate {
+        tasks.withType<JavaCompile>().configureEach {
+            sourceCompatibility = JavaVersion.VERSION_17.toString()
+            targetCompatibility = JavaVersion.VERSION_17.toString()
+        }
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            compilerOptions {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            }
+        }
+        if (pluginManager.hasPlugin("com.android.library")) {
+            configure<com.android.build.gradle.LibraryExtension> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
@@ -22,3 +49,4 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
